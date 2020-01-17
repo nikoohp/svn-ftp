275a1610d36fa821929c657116d4e4a32a853fb7
nikoohp <nikoohp@outlook.com>
2020-01-17 10:25:41 +0800

    Create dateExtends.js

---
+++ dateExtends.js
@@ -0,0 +1,23 @@
+/**
+ * 扩展日期对象
+ * 添加时间格式化方法
+ */
+Date.prototype.Format = function (fmt) {
+  let o = {
+    'M+': this.getMonth() + 1, // 月份
+    "d+": this.getDate(), //日 
+    "h+": this.getHours(), //小时 
+    "m+": this.getMinutes(), //分 
+    "s+": this.getSeconds() //秒 
+  }
+  // y出现一次或多次
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  }
+  for (var k in o) {
+    if (new RegExp("(" + k + ")").test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    }
+  }
+  return fmt;
+}
